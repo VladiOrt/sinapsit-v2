1db01d6e55b035a910e699119b440f75f2c28a82
VladiOrt <vladi_ortega@hotmail.com>
2026-03-25 20:10:52 -0600

    actualzacip

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "Resvera-tagger";
+import { componentTagger } from "Lovable-tagger";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/0
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
